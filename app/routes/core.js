22a83c925ccdc62a46664c8988e07500efe6f326
Alexis Tyler <xo@wvvw.me>
2015-11-13 13:14:14 +1030

    add search and torrent pages

--- app/routes/core.js
+++ app/routes/core.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    mongoose = require('mongoose'),
     Torrent = require('models/Torrent.js'),
     Category = require('models/Category.js');
 
@@ -16,6 +17,34 @@ module.exports = (function() {
 
     app.get('/', function(req, res){
         Category.find().exec(function(err, categories){
+            if(err){ console.log(err); }
+            res.render('index',{
+                categories: categories
+            });
+        });
+    });
+
+    app.get('/search', function(req, res){
+        Torrent.find().exec(function(err, torrents){
+            if(err){ console.log(err); }
+            res.render('search',{
+                torrents: torrents
+            });
+        });
+    });
+
+    app.get('/torrent/:infoHash', function(req, res){
+        Torrent.findOne({infoHash: req.params.infoHash}).exec(function(err, torrent){
+            if(err){ console.log(err); }
+            res.render('torrent',{
+                torrent: torrent
+            });
+        });
+    });
+
+    app.post('/search', function(req, res){
+        // var search = req.query;
+        Torrent.find().exec(function(err, categories){
             if(err){ console.log(err); }
             res.render('index',{
                 categories: categories
