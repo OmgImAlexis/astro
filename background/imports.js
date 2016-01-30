fe6ff2c24b555537438b1e3c380f021326377a2c
Aidan Harris <mail@aidanharris.io>
2016-01-30 06:07:01 +0000

    Configure Bunyan to enable logging in imports.js and providers

--- background/imports.js
+++ background/imports.js
@@ -5,6 +5,24 @@ var nconf = require('nconf');
 
 var bunyan = require('bunyan');
 
+// Sets up Bunyan to log to the same file as the BitCannon Server - Could this cause problems?
+var log = bunyan.createLogger({
+    name: 'Bitcannon',
+    version: require('../package.json').version,
+    streams: [
+        {
+            level: 'info',
+            stream: process.stdout // log INFO and above to stdout
+        }, {
+            level: 'error',
+            // log ERROR and above to a file
+            path: (
+                path.resolve(nconf.get('logs:location')).substr(0,2) === './'
+            ) ? path.resolve('../' + nconf.get('logs:location')) : nconf.get('logs:location')
+        }
+    ]
+});
+
 nconf.use('memory');
 nconf.argv().env('__').file({
     file: path.resolve(__dirname + '/../config.json')
