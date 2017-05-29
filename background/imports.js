119928cdd1f345f029037cb57111acbf27037d42
Alexis Tyler <xo@wvvw.me>
2017-05-29 21:58:49 +0930

    move mongodb to import

--- background/imports.js
+++ background/imports.js
@@ -8,8 +8,11 @@
 
 import path from 'path';
 import cluster from 'cluster';
-import nconf from 'nconf';
 import {cpus} from 'os';
+
+import nconf from 'nconf';
+import outdent from 'outdent';
+import {MongoClient} from 'mongodb';
 
 import Log from './logging';
 
@@ -20,26 +23,26 @@ nconf.argv().file({
     file: path.resolve(__dirname, '../config.json')
 });
 
- const log = new Log('imports');
+const log = new Log('imports');
 
- const providers = [];
- const workers = [];
+const providers = [];
+const workers = [];
 
- const loadProvider = provider => {
-     if (provider) {
-         this.provider = provider;
-     }
-     log.info(`Loading provider ${this.provider}`);
-     try {
-         require(`${__dirname}/providers/${this.provider}`);
-     } catch (err) {
-         log.info(outdent`
+const loadProvider = provider => {
+    if (provider) {
+        this.provider = provider;
+    }
+    log.info(`Loading provider ${this.provider}`);
+    try {
+        require(`${__dirname}/providers/${this.provider}`);
+    } catch (err) {
+        log.info(outdent`
              An error occurred loading the ${this.provider} provider.
              The error is as follows: ${err.message || ''}
          `);
-         log.trace(err);
-     }
- };
+        log.trace(err);
+    }
+};
 
 /**
  * The idea here is to use the Node.js {@link https://nodejs.org/api/cluster.html|cluster} module
@@ -48,117 +51,115 @@ nconf.argv().file({
  * process multiple archives at a time. However if this code is io bound rather than CPU bound it
  * might make no difference at all. Further testing is required.
  */
- if (cluster.isMaster) {
-     if (process.platform.startsWith('win')) {
-         require('app-module-path').addPath(`${__dirname.substring(0, __dirname.lastIndexOf('\\'))}\\app`);
-     } else {
-         require('app-module-path').addPath(`${__dirname.substring(0, __dirname.lastIndexOf('/'))}/app`);
-     }
+if (cluster.isMaster) {
+    if (process.platform.startsWith('win')) {
+        require('app-module-path').addPath(`${__dirname.substring(0, __dirname.lastIndexOf('\\'))}\\app`);
+    } else {
+        require('app-module-path').addPath(`${__dirname.substring(0, __dirname.lastIndexOf('/'))}/app`);
+    }
 
-     if (nconf.get('torrents:whitelist:enabled') && nconf.get('torrents:blacklist:enabled')) {
-         log.error('You cannot use the whitelist and the blacklist at the same time!');
-         throw new Error('You cannot use the whitelist and the blacklist at the same time!');
-     }
+    if (nconf.get('torrents:whitelist:enabled') && nconf.get('torrents:blacklist:enabled')) {
+        log.error('You cannot use the whitelist and the blacklist at the same time!');
+        throw new Error('You cannot use the whitelist and the blacklist at the same time!');
+    }
 
-     const MongoClient = require('mongodb').MongoClient;
+    if (nconf.get('database:mongodb:enabled')) {
+        MongoClient.connect(`mongodb://${nconf.get('database:mongodb:host')}:${nconf.get('database:mongodb:port')}/${nconf.get('database:mongodb:collection')}`, (err, db) => {
+            if (err) {
+                log.warn('Cannot connect to mongodb, please check your config.json');
+                throw new Error('Cannot connect to mongodb, please check your config.json');
+            }
+            db.close();
+        });
+    } else {
+        log.warn('No database is enabled, please check your config.json');
+        throw new Error('No database is enabled, please check your config.json');
+    }
 
-     if (nconf.get('database:mongodb:enabled')) {
-         MongoClient.connect(`mongodb://${nconf.get('database:mongodb:host')}:${nconf.get('database:mongodb:port')}/${nconf.get('database:mongodb:collection')}`, (err, db) => {
-             if (err) {
-                 log.warn('Cannot connect to mongodb, please check your config.json');
-                 throw new Error('Cannot connect to mongodb, please check your config.json');
-             }
-             db.close();
-         });
-     } else {
-         log.warn('No database is enabled, please check your config.json');
-         throw new Error('No database is enabled, please check your config.json');
-     }
+    let newThread; // eslint-disable-line prefer-const
 
-     let newThread; // eslint-disable-line prefer-const
+    const messageHandler = function(msg) {
+        log.info(`Master ${process.pid} received message from worker ${this.process.pid}.`, msg);
+        if ('provider' in msg) {
+            if ('name' in msg.provider) {
+                if ('next' in msg.provider) {
+                    setTimeout(() => {
+                        newThread(msg.provider.name);
+                    }, (msg.provider.next + 1000));
+                }
+            }
+        }
+    };
 
-     const messageHandler = function(msg) {
-         log.info(`Master ${process.pid} received message from worker ${this.process.pid}.`, msg);
-         if ('provider' in msg) {
-             if ('name' in msg.provider) {
-                 if ('next' in msg.provider) {
-                     setTimeout(() => {
-                         newThread(msg.provider.name);
-                     }, (msg.provider.next + 1000));
-                 }
-             }
-         }
-     };
-
-     newThread = provider => {
-         let availableCluster = false;
-         let threadNum;
-         if (workers.length < numCPUs) {
-             availableCluster = true;
-             workers.push(cluster.fork());
-             threadNum = workers.length - 1;
-         } else {
-             for (threadNum = 0; threadNum < workers.length; threadNum++) {
-                 if (!workers[threadNum]) {
-                     availableCluster = true;
-                     workers[threadNum] = cluster.fork();
-                     break;
-                 }
-             }
-         }
-         if (availableCluster) {
-             if (providers.length !== 0) {
-                 providers.push(String(provider));
-                 provider = providers.splice(0, 1);
-             }
-             if (provider) {
+    newThread = provider => {
+        let availableCluster = false;
+        let threadNum;
+        if (workers.length < numCPUs) {
+            availableCluster = true;
+            workers.push(cluster.fork());
+            threadNum = workers.length - 1;
+        } else {
+            for (threadNum = 0; threadNum < workers.length; threadNum++) {
+                if (!workers[threadNum]) {
+                    availableCluster = true;
+                    workers[threadNum] = cluster.fork();
+                    break;
+                }
+            }
+        }
+        if (availableCluster) {
+            if (providers.length !== 0) {
+                providers.push(String(provider));
+                provider = providers.splice(0, 1);
+            }
+            if (provider) {
                  // Receive messages from this worker and handle them in the master process.
-                 workers[threadNum].on('message', messageHandler);
-                 workers[threadNum].send({provider: String(provider)});
-             } else {
-                 workers[threadNum].kill();
-                 workers[threadNum] = undefined;
-             }
-         } else if (provider) {
-             providers.push(String(provider));
-         }
-     };
+                workers[threadNum].on('message', messageHandler);
+                workers[threadNum].send({provider: String(provider)});
+            } else {
+                workers[threadNum].kill();
+                workers[threadNum] = undefined;
+            }
+        } else if (provider) {
+            providers.push(String(provider));
+        }
+    };
 
-     for (const provider in nconf.get('providers')) {
-         if (provider === 'provider') {
-             log.warn('You cannot directly use the provider file. This is a helper file for other providers.');
-         }
+    for (const provider in nconf.get('providers')) {
+        if (provider === 'provider') {
+            log.warn('You cannot directly use the provider file. This is a helper file for other providers.');
+        }
          // For single core processors we don't fork, we just load all of the providers
          // and let Node do its thing.
-         if (!nconf.get(`providers:${provider}:enabled`)) {
-             continue;
-         }
-         if (numCPUs === 1) {
-             loadProvider(String(provider));
-         } else {
-             newThread(String(provider));
-         }
-     }
-     cluster.on('exit', (worker, code, signal) => {
-         if (signal) {
-             log.info(`worker was killed by signal: ${signal}`);
-         } else if (code !== 0) {
-             log.info(`worker exited with error code: ${code}`);
-         } else if (worker.exitedAfterDisconnect !== true) {
-             log.info('worker success!');
-             workers[worker.id - 1] = undefined;
-             newThread();
-         }
-     });
- } else {
-     log.info('Worker ' + process.pid + ' has started.');
+        if (!nconf.get(`providers:${provider}:enabled`)) {
+            continue;
+        }
+        if (numCPUs === 1) {
+            loadProvider(String(provider));
+        } else {
+            newThread(String(provider));
+        }
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        if (signal) {
+            log.info(`worker was killed by signal: ${signal}`);
+        } else if (code !== 0) {
+            log.info(`worker exited with error code: ${code}`);
+        } else if (worker.exitedAfterDisconnect !== true) {
+            log.info('worker success!');
+            workers[worker.id - 1] = undefined;
+            newThread();
+        }
+    });
+} else {
+    log.info('Worker ' + process.pid + ' has started.');
 
      // Receive messages from the master process.
-     process.on('message', msg => {
-         if (msg.provider) {
-             loadProvider(msg.provider);
-         } else {
-             log.info('Worker ' + process.pid + ' received message from master.', msg);
-         }
-     });
- }
+    process.on('message', msg => {
+        if (msg.provider) {
+            loadProvider(msg.provider);
+        } else {
+            log.info('Worker ' + process.pid + ' received message from master.', msg);
+        }
+    });
+}
