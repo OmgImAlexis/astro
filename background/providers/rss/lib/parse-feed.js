bb9c8d47241719502c4ff0e8c20ac8d7f45912e3
Aidan Harris <mail@aidanharris.io>
2017-05-31 17:05:17 +0100

    Remove namespace from namespaced torrents and emit torrent as an object

--- background/providers/rss/lib/parse-feed.js
+++ background/providers/rss/lib/parse-feed.js
@@ -1,42 +1,88 @@
 import {EventEmitter} from 'events';
 import {promisifyAll} from 'bluebird';
 import xml2js from 'xml2js';
-import {generateAtomFeed} from '../tests/generate-test-data';
 
 const {parseStringAsync} = promisifyAll(xml2js);
 
-const torrentTags = [
-    'contentLength',
-    'infoHash',
-    'seeds',
-    'peers'
-];
+const getFeedType = feed => {
+    // <torrent xmlns="http://xmlns.ezrss.it/0.1/">
+    //    <infoHash>...</infoHash>
+    // </torrent>
+    if ('torrent' in feed.rss.channel[0]) {
+        if (typeof feed.rss.channel[0].torrent[0].$.xmlns !== 'undefined') {
+            return {
+                torrents: feed.rss.channel[0].torrent,
+                items: ('item' in feed.rss.channel[0]) ? feed.rss.channel[0].item : feed.rss.channel[0].torrent,
+                namespace: ''
+            };
+        }
+    }
+
+    // <rss xmlns:torrent="http://xmlns.ezrss.it/0.1/">
+    //    <torrent:infoHash>...</torrent:infoHash>
+    // </rss>
+    if (typeof (feed.rss.$['xmlns:torrent']) !== 'undefined') {
+        return {
+            torrents: feed.rss.channel[0].item,
+            items: feed.rss.channel[0].item,
+            namespace: 'torrent:'
+        };
+    }
+
+    // <rss xmlns:atom="http://www.w3.org/2005/Atom">
+    //    ...
+    //    <enclosure url="http://example.com/example.torrent"
+    //               type="application/x-bittorrent"
+    //               length="10000"
+    //    />
+    //    ...
+    // </rss>
+    return {
+        torrents: feed.rss.channel[0].item,
+        items: feed.rss.channel[0].item,
+        namespace: ''
+    };
+};
 
 class ParseFeed extends EventEmitter {
     constructor(feed) {
         super();
         parseStringAsync(feed).then(xml => {
-            if ('rss' in xml === false) {
-                return this.emit('error', new Error('NotAnRSSFeed'));
+            if ('rss' in xml === false || 'channel' in xml.rss === false) {
+                this.emit('error', new Error('NotAnRSSFeed'));
             }
 
-            xml.rss.channel[0].item.forEach(item => {
-                this.emit('torrent', item);
-            });
+            const {torrents, items, namespace} = getFeedType(xml);
+
+            for (let i = 0; i < torrents.length; i++) {
+                // Remove the namespace from namespaced feeds so
+                // torrent:infoHash becomes infoHash.
+                if (namespace !== '') {
+                    for (const prop in torrents[i]) {
+                        if (prop.startsWith(namespace)) {
+                            const newprop = prop.substring(namespace.length);
+                            torrents[i][newprop] = torrents[i][prop];
+                            delete torrents[i][prop];
+                        }
+                    }
+                }
+                // Ocassionally there are RSS feeds that take the following format:
+                // <torrent>
+                //  ...
+                // </torrent>
+                // <item>
+                //  ...
+                // </item>
+                // AFAIK this is non-standard (http://www.bittorrent.org/beps/bep_0036.html)
+                // but sometimes contains useful information such as a description. In this
+                // case we return an object containing both the torrent and item tag
+                this.emit('torrent',
+                (torrents[i] === items[i]) ? {torrent: torrents[i]} : {torrent: torrents[i], item: items[i]});
+            }
         }).catch(err => {
-            return this.emit('error', err);
+            this.emit('error', err);
         });
     }
 }
-
-generateAtomFeed().then(f => {
-    new ParseFeed(f).on('torrent', torrent => {
-        console.log(torrent);
-    }).on('error', err => {
-        throw err;
-    });
-}).catch(e => {
-    throw new Error(e);
-});
 
 export default ParseFeed;
