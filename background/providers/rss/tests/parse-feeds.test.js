4120df666ee771bdcc26c8e1eb1574a79c9e92fc
Aidan Harris <mail@aidanharris.io>
2017-05-31 22:47:04 +0100

    Forgot to `git add` the tests...

---
+++ background/providers/rss/tests/parse-feeds.test.js
@@ -0,0 +1,79 @@
+/* eslint guard-for-in: 0 max-nested-callbacks: 0 */
+
+import {test} from 'ava';
+
+import {default as ParseFeed} from '../lib/parse-feed';
+
+import {
+    generateAtomFeed,
+    generateTorrentNamespacedFeed,
+    generateTorrentFeed,
+    generateMixedTorrentFeed
+} from './generate-test-data';
+import {default as torrents} from './testdata';
+
+const feeds = [generateAtomFeed, generateTorrentNamespacedFeed, generateTorrentFeed, generateMixedTorrentFeed];
+
+feeds.forEach(fn => {
+    fn().then(feed => {
+        test.cb(`The ${fn.name} feed should have ${torrents.length} elements`, t => {
+            let _count = 0;
+            new ParseFeed(feed).on('torrent', () => {
+                _count++;
+            }).on('end', () => {
+                t.is(_count, torrents.length);
+                t.end();
+            });
+        });
+    });
+});
+
+test.cb('The generateMixedTorrentFeed feed should contain "item" objects', t => {
+    generateMixedTorrentFeed().then(feed => {
+        let _count = 0;
+        new ParseFeed(feed).on('torrent', torrent => {
+            if ('item' in torrent) {
+                _count++;
+            }
+        }).on('end', () => {
+            t.is(_count, torrents.length);
+            t.end();
+        });
+    });
+});
+
+const torrentTags = [
+    'title',
+    'category',
+    'contentLength',
+    'seeds',
+    'peers',
+    'enclosure'
+];
+
+torrentTags.forEach(tag => {
+    feeds.forEach(fn => {
+        if (fn.name === 'generateAtomFeed') {
+            if (tag === 'seeds' || tag === 'peers' || tag === 'category' || tag === 'contentLength') {
+                return;
+            }
+        }
+        fn().then(feed => {
+            let _count = 0;
+            test.cb(`The ${fn.name} feeds "${tag}" tags should be the same as torrents`, t => {
+                new ParseFeed(feed).on('torrent', torrent => {
+                    if (torrent.torrent[tag] instanceof Object) {
+                        for (const prop in torrent.torrent[tag]) {
+                            t.is(torrent.torrent[tag][prop], torrents[_count][tag][prop]);
+                        }
+                    } else {
+                        t.is(torrent.torrent[tag], torrents[_count][tag]);
+                    }
+                    _count++;
+                }).on('end', () => {
+                    t.end();
+                });
+            });
+        });
+    });
+});
