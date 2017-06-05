d99d58993afac7978ca70e1c0a119537e6f06817
Alexis Tyler <xo@wvvw.me>
2017-06-05 21:39:44 +0930

    add api and torrent tests

--- app/routes/api.js
+++ app/routes/api.js
@@ -30,8 +30,8 @@ router.use((req, res, next) => {
     return next();
 });
 
-router.get('/stats', (req, res) => {
-    res.send('Welcome to the Astro api');
+router.get('/stats', (req, res, next) => {
+    next(new HTTPError.NotImplemented());
 });
 
 router.get('/browse', async (req, res, next) => {
---
+++ test/routes/api.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import request from 'supertest';
+import test from 'ava';
+import {version} from '../../package';
+import {makeApp} from '../helpers';
+
+test.before(() => {
+    mongoose.Promise = Promise;
+    mongoose.connect('mongodb://localhost:27017/astro-test-db');
+});
+
+test.after.always(() => {
+    mongoose.disconnect();
+});
+
+test('should return 200', async t => {
+    const app = makeApp();
+    const res = await request(app).get('/api/');
+    t.is(res.status, 200);
+    t.is(res.body.message, 'Welcome to the Astro API.');
+    t.is(res.body.version, version);
+});
+
+test('should return NotImplemented', async t => {
+    const app = makeApp();
+    const res = await request(app).get('/api/stats');
+    t.is(res.status, 501);
+    t.is(res.body.error, 'Not Implemented');
+});
